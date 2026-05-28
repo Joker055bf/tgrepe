11355bb62d843f0da55b7758ed31901224da8543
Joker055bf <140885401+Joker055bf@users.noreply.github.com>
2026-05-28 19:26:15 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,13 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => caches.delete(cacheName))
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 // Handle notification click to focus or open the barn manager app
